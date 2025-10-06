81055c5153a1c037abcb1ec36c4379ff1320c40a
yewen <2492465270@qq.com>
2025-10-06 14:58:10 +0800

    修改premake5.lua，使其导出dllexport

--- premake5.lua
+++ premake5.lua
@@ -25,6 +25,11 @@ project "ImGui"
 		systemversion "latest"
 		cppdialect "C++17"
 
+        defines
+        {
+            "IMGUI_API=__declspec(dllexport)"
+        }
+
 	filter "system:linux"
 		pic "On"
 		systemversion "latest"
